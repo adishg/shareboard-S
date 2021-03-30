4fcfa3faf631902d8ed1fa3b5f601901e1a13b2e
hari <hari.prasad@gingerwebs.in>
2021-03-30 16:50:08 +0530

    Auth Routes

---
+++ src/auth/Auth.component.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const AuthForm = styled.form `
+display:flex;
+align-items: center;
+flex-direction: 50%;
+width: 50%'
+margin: 0 auto;
+`
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ExcalidrawApp from "./excalidraw-app";
+import {mount, route} from 'navi';
+import {Router} from 'react-navi';
+
 
 import "./excalidraw-app/pwa";
 import "./excalidraw-app/sentry";
+import LoginPage from "./auth/LoginPage";
+import RegisterPage from "./auth/RegisterPage";
+
+const routes = mount({
+    "/": route({
+        title:"Shareboard",
+        view: <ExcalidrawApp />
+    }),
+    "/login": route({
+        title:"Login | Shareboard",
+        view: <LoginPage/>
+    }),
+    "/register": route({
+        title:"Login | Shareboard",
+        view: <RegisterPage/>
+    })
+})
 
 window.__EXCALIDRAW_SHA__ = process.env.REACT_APP_GIT_SHA;
 
-ReactDOM.render(<ExcalidrawApp />, document.getElementById("root"));
+ReactDOM.render(<Router routes = {routes}/>,
+    document.getElementById("root")
+);
